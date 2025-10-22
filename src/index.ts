e0dfff9adb567485d33c6e7300aa5be237ecf8a6
Gavin Smith <me@gavinhsmith.com>
2025-10-22 18:33:23 -0500

    [test] Added test client at root, just to check launch/fail

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.info("Hello, World!");
+import { getBotClient, logger } from "./bot";
+
+// Attempt to connect to the bot system, and close the connection after.
+getBotClient().then((client) => {
+  logger.info("Test complete, closing bot client...");
+  client.destroy();
+}).catch(logger.error);
