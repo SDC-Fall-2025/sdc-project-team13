bc953f34155e0f8b61547705bae86f5a26fa737d
Gavin Smith <me@gavinhsmith.com>
2025-10-22 19:30:21 -0500

    [feat] Initiated winston log tool to create and use different loggers with rotating files and console out.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { getBotClient, logger } from "./bot";
+import { getBotClient, logger as botLog } from "./bot";
+import { logger } from "./tools/log";
+
+// Start the application.
+logger.info("Starting the program...");
 
 // Attempt to connect to the bot system, and close the connection after.
 getBotClient().then((client) => {
-  logger.info("Test complete, closing bot client...");
+  botLog.info("Test complete, closing bot client...");
   client.destroy();
-}).catch(logger.error);
+  
+  logger.info("Exit with code 0.");
+  process.exit(0);
+}).catch(botLog.error);
--- src/tools/log.ts
+++ src/tools/log.ts
@@ -1,5 +1,28 @@
 // Import modules for logger.
 import { createLogger, format, transports } from "winston";
+import "winston-daily-rotate-file";
+
+/**
+ * Create a rotating file transport for the loggers.
+ * 
+ * @param name The name of the log file (should be a word that describes what this log file has).
+ * @param level The min level to write.
+ * @returns An instance of a transport for winston loggers.
+ */
+function createRotatingFileTransport(name: string, level: string) {
+  // Log Location: ./logs/YYYY-MM-DD-HH_<name>.log
+  return new transports.DailyRotateFile({
+    dirname: `./logs`,
+    filename: `%DATE%_${name}.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    maxSize: "20m",
+    level
+  });
+}
+
+// The full log file and issue log file.
+const fullLogFile = createRotatingFileTransport("full", "verbose");
+const issueLogFile = createRotatingFileTransport("issues", "warning");
 
 /**
  * Creates a new named logger.
@@ -18,6 +41,13 @@ export function createNewLogger(name: string, level: string) {
         return `${timestamp} [${label}] ${level}: ${message}`;
       })
     ),
-    transports: [new transports.Console()]
+    transports: [
+      fullLogFile,
+      issueLogFile,
+      new transports.Console()
+    ]
   })
 }
+
+/** The main, non-specfic logger. */
+export const logger = createNewLogger("main", "info");
