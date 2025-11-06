0ac4ff885a0a8733d88aa06e3324b97eaa68fc43
connorfurby <111527458+connorfurby@users.noreply.github.com>
2025-11-05 18:19:34 -0600

    GitHub Commands - github repo and github commits

--- src/bot/commands/registry.ts
+++ src/bot/commands/registry.ts
@@ -1,15 +1,24 @@
+
 import { helloCommand, handleHello } from "./hello";
 import { randomCommand, handleRandom } from "./random";
 import { flipCoinCommand, handleFlipCoin } from "./flipCoin";
+import { githubCommand, handleGithub } from "./github";
 
 /**
  * Central registry for slash commands:
  * - commandDefinitions: used by registerCommands.ts to sync with Discord
  * - commandHandlers: used at runtime to route interactions by name
  */
-export const commandDefinitions = [helloCommand, randomCommand, flipCoinCommand];
+export const commandDefinitions = [
+  helloCommand,
+  randomCommand,
+  flipCoinCommand,
+  githubCommand
+];
+
 export const commandHandlers = new Map([
-    [helloCommand.name, handleHello],
-    [randomCommand.name, handleRandom],
-    [flipCoinCommand.name, handleFlipCoin]
+  [helloCommand.name, handleHello],
+  [randomCommand.name, handleRandom],
+  [flipCoinCommand.name, handleFlipCoin],
+  [githubCommand.name, handleGithub]
 ]);
