79b8f7334dac3fcdeeac123e566effba5b18abfa
Cash Pergande <cashpergande@Cashs-MacBook-Air.local>
2025-11-10 20:47:33 -0600

    Added basic commands for database integration

--- src/bot/commands/registry.ts
+++ src/bot/commands/registry.ts
@@ -1,14 +1,24 @@
 import { helloCommand, handleHello } from "./hello";
 import { randomCommand, handleRandom } from "./random";
+import { createCommand, handleCreate } from "./create";
+import { groupCommand, handleGroup } from "./group";
+import { joinCommand, handleJoin } from "./join";
+import { kickCommand, handleKick } from "./kick";
+import { manageCommand, handleManage } from "./manage";
 
 /**
  * Central registry for slash commands:
  * - commandDefinitions: used by registerCommands.ts to sync with Discord
  * - commandHandlers: used at runtime to route interactions by name
  */
-export const commandDefinitions = [helloCommand, randomCommand];
+export const commandDefinitions = [helloCommand, randomCommand, createCommand, groupCommand, joinCommand, kickCommand, manageCommand];
 export const commandHandlers = new Map([
     [helloCommand.name, handleHello],
-    [randomCommand.name, handleRandom]
+    [randomCommand.name, handleRandom],
+    [createCommand.name, handleCreate],
+    [groupCommand.name, handleGroup],
+    [joinCommand.name, handleJoin],
+    [kickCommand.name, handleKick],
+    [manageCommand.name, handleManage]
 ]);
 
